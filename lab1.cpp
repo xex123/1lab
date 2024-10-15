36043627765ce2597792f7694f8077edab042553
qq <qq@gmail.com>
2024-10-15 11:40:10 +0300

    3

--- lab1.cpp
+++ lab1.cpp
@@ -5,26 +5,47 @@
 
 int main() {
     setlocale(LC_ALL, "Rus");
-    int size = 0;
-    int* mass = (int*)malloc(size * sizeof(int));
-    int max = INT_MIN;
-    int min = INT_MAX;
-    printf("Введите количество элементов: ");
-    scanf_s("%d", &size);
-    for (int i = 0; i < size; i++) {
-        mass[i] = -1000 + rand() % 2001;
-        printf("%d ", mass[i]);
-        if (mass[i] < min) {
-            min = mass[i];
-        }
-        if (mass[i] > max) {
-            max = mass[i];
-        }
+    
+    int rows, cols;
+
+    printf("Введите количество строк: ");
+    scanf_s("%d", &rows);
+    printf("Введите количество столбцов: ");
+    scanf_s("%d", &cols);
+
+    int** array = (int**)malloc(rows * sizeof(int*));
+    for (int i = 0; i < rows; i++) {
+        array[i] = (int*)malloc(cols * sizeof(int));
     }
 
-    printf("\nМаксимальное: %d\nМинимальное: %d\nИх разница = %d\n", max, min, max - min);
+    printf("Массив:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            array[i][j] = rand() % 150 - 75;
+            printf("%5d", array[i][j]);
+        }
+        printf("\n");
+    }
 
-    free(mass);
+    for (int i = 0; i < rows; i++) {
+        int rowSum = 0;
+        for (int j = 0; j < cols; j++) {
+            rowSum += array[i][j];
+        }
+        printf("Сумма строки: %d\n", rowSum);
+    }
 
+    for (int j = 0; j < cols; j++) {
+        int columnSum = 0;
+        for (int i = 0; i < rows; i++) {
+            columnSum += array[i][j];
+        }
+        printf("Сумма столбца: %d\n", columnSum);
+    }
+
+    for (int i = 0; i < rows; ++i) {
+        free(array[i]);
+    }
+    free(array);
     return 0;
 }
