81f0ef0ac89e2f015f428a4c9abc504fbb813df1
qq <qq@gmail.com>
2024-10-15 14:34:11 +0300

    4

--- lab1.cpp
+++ lab1.cpp
@@ -2,50 +2,55 @@
 #include <stdlib.h>
 #include <limits.h>
 #include <locale.h>
+#include <string.h>
+
+typedef struct {
+    char lastName[50];
+    char firstName[50];
+    int age;
+    int studentID;
+} Student;
+
+void findStudent(Student students[], int size, const char* lastName, const char* firstName) {
+    int found = 0;
+    for (int i = 0; i < size; i++) {
+        if (strcmp(students[i].lastName, lastName) == 0 && strcmp(students[i].firstName, firstName) == 0) {
+            printf("Студент найден:\n");
+            printf("Фамилия: %s\n", students[i].lastName);
+            printf("Имя: %s\n", students[i].firstName);
+            printf("Возраст: %d\n", students[i].age);
+            printf("Номер зачетной книжки: %d\n", students[i].studentID);
+            found = 1;
+        }
+    }
+    if (!found) {
+        printf("Студент не найден\n");
+    }
+}
 
 int main() {
+
     setlocale(LC_ALL, "Rus");
-    
-    int rows, cols;
 
-    printf("Введите количество строк: ");
-    scanf_s("%d", &rows);
-    printf("Введите количество столбцов: ");
-    scanf_s("%d", &cols);
+    Student students[] = {
+        {"Корякин", "Илья", 20, 1001},
+        {"Шабанов", "Максим", 22, 1002},
+        {"Давыдов", "Денис", 21, 1003}
+    };
+    int size = sizeof(students) / sizeof(students[0]);
 
-    int** array = (int**)malloc(rows * sizeof(int*));
-    for (int i = 0; i < rows; i++) {
-        array[i] = (int*)malloc(cols * sizeof(int));
-    }
+    char lastName[50];
+    char firstName[50];
 
-    printf("Массив:\n");
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            array[i][j] = rand() % 150 - 75;
-            printf("%5d", array[i][j]);
-        }
-        printf("\n");
-    }
+    printf("Введите фамилию студента: ");
+    fgets(lastName, sizeof(lastName), stdin);
+    lastName[strcspn(lastName, "\n")] = 0;
 
-    for (int i = 0; i < rows; i++) {
-        int rowSum = 0;
-        for (int j = 0; j < cols; j++) {
-            rowSum += array[i][j];
-        }
-        printf("Сумма строки: %d\n", rowSum);
-    }
+    printf("Введите имя студента: ");
+    fgets(firstName, sizeof(firstName), stdin);
+    firstName[strlen(firstName) - 1] = 0;
 
-    for (int j = 0; j < cols; j++) {
-        int columnSum = 0;
-        for (int i = 0; i < rows; i++) {
-            columnSum += array[i][j];
-        }
-        printf("Сумма столбца: %d\n", columnSum);
-    }
+    findStudent(students, size, lastName, firstName);
 
-    for (int i = 0; i < rows; ++i) {
-        free(array[i]);
-    }
-    free(array);
     return 0;
 }
